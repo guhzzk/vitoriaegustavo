0fbb19721e603a797a764aba128da95e32967349
guhzzk <rodrigues.freitas.gustavo@escola.pr.gov.br>
2026-06-15 12:15:58 -0300

    Update service worker to include new icons
    Removed the old caching logic and added new icon files.

--- sw.js
+++ sw.js
@@ -4,36 +4,6 @@ const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
-
-// instala cache
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES);
-    })
-  );
-});
-
-// ativa cache novo
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
-// intercepta requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    })
-  );
-});
