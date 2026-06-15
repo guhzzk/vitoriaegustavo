b03dc2f1a6ec10eaf6fac5754db175cf9346e089
guhzzk <rodrigues.freitas.gustavo@escola.pr.gov.br>
2026-06-15 10:10:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,39 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("v1").then(cache=>{
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+const CACHE_NAME = "gv-love-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// instala cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES);
     })
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
+// ativa cache novo
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// intercepta requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
   );
 });
